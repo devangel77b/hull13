01ace4c0f183edd8beb67558f497372aa47fb2b8
Dennis Evangelista <evangeli@usna.edu>
2018-04-27 09:00:50 -0400

    adding test for sailtrim and gps logging

---
+++ TESTS/integrated/gpsnlog/main.cpp
@@ -0,0 +1,67 @@
+#include "mbed.h"
+#include "rtos.h"
+#include "stdio.h"
+#include "gps.h"
+#include "logger.h"
+#include "xbee.h"
+#include "unity.h"
+
+Serial pc(USBTX,USBRX);
+Gps gps(PC_2, PC_3);
+Logger logger(PC_10,PC_11,9600);
+Xbee telem(PC_12,PD_2,57600);
+
+float longitude;
+float latitude;
+float course;
+float speed;
+int year;
+byte month;
+byte day;
+byte hour;
+byte minute;
+byte second;
+byte hundredths; 
+unsigned long fix_age;
+unsigned long gdate;
+unsigned long dtime; 
+
+int main(){
+  pc.printf("Sailbot 13.2.0\n\r");
+  pc.printf("Integrated test of GPS and logging\n\r");
+  pc.printf("Take a walk!\n\r"); 
+
+  logger.printf("Sailbot 13.2.0\n");
+  logger.printf("Integrated test of GPS and logging\n");
+  logger.printf("OpenLogger output\n");
+  
+  telem.printf("Sailbot 13.2.0\n");
+  telem.printf("Integrated test of GPS and logging\n");
+  telem.printf("Xbee output\n");
+
+  while(1){
+    gps.get_datetime(&gdate, &gtime &fix_age);
+    pc.printf("%d %d ",gdate,gtime);
+    logger.printf("%d %d ",gdate,gtime);
+    telem.printf("%d %d ",gdate,gtime);
+
+    gps.crack_datetime(&year,&month,&day,&hour,&minute,&second,&hundredths,&fix_age);
+    pc.printf("%d%02d%02d %02d:%02d:%02d.%02d ",year,month,day,hour,minute,second,hundredths);
+    logger.printf("%d%02d%02d %02d:%02d:%02d.%02d ",year,month,day,hour,minute,second,hundredths);
+    telem.printf("%d%02d%02d %02d:%02d:%02d.%02d ",year,month,day,hour,minute,second,hundredths); 
+    
+    gps.f_get_position(&latitude, &longitude, &fix_age);
+    course = gps.f_course();
+    speed = gps.f_speed_knots();
+    pc.printf("%f %f C%05.1f S%4.1\n\r",longitude,latitude,course,speed);
+    logger.printf("%f %f C%05.1f S%4.1\n",longitude,latitude,course,speed);
+    telem.printf("%f %f C%05.1f S%4.1\n",longitude,latitude,course,speed);
+
+
+  
+    
+    Thread::wait(1000); 
+  }
+
+  
+}
---
+++ TESTS/integrated/sailtrim/main.cpp
@@ -0,0 +1,50 @@
+#include "mbed.h"
+#include "rtos.h"
+#include "stdio.h"
+#include "mainsail.h"
+#include "windbirdie.h"
+#include "unity.h"
+
+Serial pc(USBTX,USBRX);
+Mainsail mainsail(PC_6);
+WindBirdie windbirdie(PC_4);
+
+Thread sailtrim_thread;
+
+void sailtrim_callback(void){
+  float rdeg = windbirdie.rdeg;
+  float sailtrim;
+
+  // starboard tack
+  if ((0 <= rdeg) && (rdeg < 90.0)) { sailtrim = 15.0*rdeg/90.0; }
+  else if ((90.0 <= rdeg) && (rdeg < 120.0)) { sailtrim = 15.0+rdeg-90.0; }
+  else if ((rdeg <= 120.0) && (rdeg < 180.0)) { sailtrim = 45.0; }
+
+  // port tack
+  if ((0 >= rdeg) && (rdeg > -90.0)) { sailtrim = 15.0*rdeg/90.0; }
+  else if ((-90.0 >= rdeg) && (rdeg > -120.0)) { sailtrim = -(15.0-rdeg-90.0); }
+  else if ((rdeg >= -120.0) && (rdeg > -180.0)) { sailtrim = -45.0; }
+
+  pc.printf("Birdie %f deg relative, trimming to %f\n\r",rdeg,sailtrim);
+  mainsail.write(sailtrim);
+  Thread::wait(100); 
+}
+
+int main(){
+  pc.printf("Integrated test of sail trimming\n\r");
+  
+  pc.printf("Starting sail trim thread\n\r");
+  sailtrim_thread.attach(&sailtrim_callback);
+
+  for (int i=60; i>0; i--){
+    pc.printf("%d s remaining in test\n\r",i);
+    Thread::wait(1000); 
+  }
+
+  pc.printf("Stopping sail trim thread\n\r");
+  sailtrim_thread.detach();
+
+  pc.printf("Did it work? (y/n) ");
+  pc.scanf("%c", &c);
+  TEST_ASSERT_TRUE_MESSAGE((c=='y'),"Integrated sail trim test failed\n\r");
+}
